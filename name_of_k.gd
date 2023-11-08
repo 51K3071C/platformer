1a2e4584897a6a22a39e053d6c8d6ce25bb3da94
S1K3071C <Gogetassgss32@gmail.com>
2023-11-08 11:15:33 -0600

    almost done
    almost done

--- name_of_k.gd
+++ name_of_k.gd
@@ -11,10 +11,6 @@ func _process(delta):
 	pass
 
 
-func _on_interact_body_entered(body):
-	if body.is_in_group("Player"):
-		body.die()
-		
 func _on_transition_body_entered(body):
 	if body.is_in_group("Player"):
-		get_tree().change_scene_to_file("res://name_of_k.tscn")
+		get_tree().change_scene_to_file("res://nonsenseland.tscn")
